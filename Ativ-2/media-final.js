146c93f9fb577bc4c0e3e2670c26621efa7ce125
Vinicius Sales de Souza <viniciussds4@gmail.com>
2025-07-02 21:41:17 -0300

    Atividades JS editado

--- Ativ-2/media-final.js
+++ Ativ-2/media-final.js
@@ -5,18 +5,18 @@ const readline = require("readline-sync");
 console.log("Média Final");
 
 let nome = readline.question("Digite nome do Aluno: ");
-let B1 = readline.questionFloat("Digite nota B1: ");
-let B2 = readline.questionFloat("Digite nota B2: ");
-let B3 = readline.questionFloat("Digite nota B3: ");
-let B4 = readline.questionFloat("Digite nota B4: ");
+let b1 = readline.questionFloat("Digite nota B1: ");
+let b2 = readline.questionFloat("Digite nota B2: ");
+let b3 = readline.questionFloat("Digite nota B3: ");
+let b4 = readline.questionFloat("Digite nota B4: ");
 
 
 //Utilizando operador aritmético
-let mediaFinal = (B1 + B2 + B3 + B4) / 4
+let mediaFinal = (b1 + b2 + b3 + b4) / 4
 
 //Saída
 console.log(`Olá aluno, ${nome}!`);
-console.log(`Sua nota final é: ${mediaFinal.toFixed(1)}.`);
+console.log(`Sua nota final é: ${mediaFinal.toFixed(1)}`);
     if (mediaFinal >= 7){
     console.log("Aprovado.");
     } else {
